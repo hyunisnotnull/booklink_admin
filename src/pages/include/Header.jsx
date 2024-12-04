c61c518e32555ddc25f925cba4ff491a9d8d935a
imbobby71 <imbobby71@gmail.com>
2024-12-04 17:31:34 +0900

    20241204

--- src/pages/include/Header.jsx
+++ src/pages/include/Header.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect }  from 'react';
+import { useNavigate } from 'react-router-dom'; 
+import { useJwt } from "react-jwt";
+import { useCookies } from 'react-cookie'; // useCookies import
 import '../../css/include/Header.css';
 
+
+
 const Header = () => {
+  
+  const navigate = useNavigate(); 
+  const [cookie, setCookie, removeCookie] =  useCookies();
+  const { decodedToken, isExpired } = useJwt(cookie.token);
+  // const decoded = jwtDecode(token);
+  
+  
+  useEffect(() => {
+    
+  }, [isExpired, cookie.token, decodedToken]);
+  
+  const signOutClickHandler = (e) => {
+    e.preventDefault();
+    // props.setULoginId('');
+    removeCookie('token');
+    navigate('/');
+  }
+
   return (
     <header className="header">
       <div className="logo">
@@ -10,8 +33,17 @@ const Header = () => {
         </a>
       </div>
       <div className="auth">
-        <a href='#none'>로그인</a>
-        <a href='#none'>회원가입</a>
+        {!isExpired ?
+          <>
+          <a href='/modify'>회원수정</a>
+          <a href='#none' onClick={signOutClickHandler} >로그아웃</a>
+          </>
+          :
+          <>
+          <a href='/signin'>로그인</a>
+          <a href='/signup'>회원가입</a>
+          </>
+          }
       </div>
     </header>
   );
